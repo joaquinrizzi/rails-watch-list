c038c9b398ddc772df9ec04a71e51eb9c5c0cc0f
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-06-26 14:08:43 +0200

    gemfile setup, bookmark model, routes and seeds
    Signed-off-by: Joaquin Soto <joaquinsotorizzi@gmail.com>

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, presence: true, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list, message: "the combination  movie/list already exists in the bookmarks" }
+  validates :movie, uniqueness: { scope: :list, message: "the combination movie/list already exists in the bookmarks" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "open-uri"
+require "json"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+poster_base_url = "https://image.tmdb.org/t/p/w500/"
+response_serialized = URI.open(url).read
+response = JSON.parse(response_serialized)
+
+response["results"].each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "#{poster_base_url}#{movie["poster_path"]}" , rating: movie["vote_average"])
+end
+
+List.create(name: "Drama")
+List.create(name: "All time favourites")
+List.create(name: "Girl Power")
+List.create(name: "Boy Power")
+List.create(name: "Never watch again")
+
+Bookmark.create(comment: "saw it at the cinema", list_id: List.first.id, movie_id: Movie.first.id)
+Bookmark.create(comment: "saw it at the cinema", list_id: List.last.id, movie_id: Movie.first.id)
+Bookmark.create(comment: "saw it at the cinema", list_id: List.first.id, movie_id: Movie.last.id)
